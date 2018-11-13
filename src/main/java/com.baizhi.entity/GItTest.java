be646c248dfeb248e3940e5ee3d5360a5710c312
zw <332385101@qq.com>
2018-11-13 20:42:43 +0800

    冲突

--- src/main/java/com.baizhi.entity/GItTest.java
+++ src/main/java/com.baizhi.entity/GItTest.java
@@ -5,4 +5,8 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("i am zhangsan");
     }
+
+    public void ad(int a, int b, int c) {
+        System.out.println("abc");
+    }
 }
